df9ae218f3ac4f058eb2d9a189d9b10498d6c511
chrisjbatts <chrisjbatts@gmail.com>
2014-11-22 21:02:15 +0000

    error fixed, tests passing, password encrypted

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -12,8 +12,8 @@ end
 
 def sign_in(email, password)
   visit '/sessions/new'
-  fill_in email, :with => email
-  fill_in password, :with => password
+  fill_in 'email', :with => email
+  fill_in 'password', :with => password
   click_button 'Sign in'
 end
 
